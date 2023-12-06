a107ff37cd6881f9c79d86cc07eff8ce9dd91f9e
ThomsonToms <117095971+ThomsonToms@users.noreply.github.com>
2023-12-05 20:07:28 -0500

    rearranged the layout of containers to make  sense

--- src/Views/HomeView.tsx
+++ src/Views/HomeView.tsx
@@ -39,6 +39,7 @@ function HomeView () {
     return(
         <div className="m-2">
             <div className="row p-2">
+                <div className="col-lg-12"> {/* full width for Featured Stocks */}
                     <div className="floatingDiv"> 
                         <h3>Featured Stocks</h3>
                         <hr/>
@@ -51,30 +52,31 @@ function HomeView () {
                         </div>
                     </div>
                 </div>
-                <div className="my-3 col-lg-12 col-sm-12">
+            </div>
+            <div className="row"> {/* new row for Personal Stocks and Your Stocks */}
+                <div className="col-lg-2 col-sm-12"> {/* adjust the size if u want */}
+                    <div className="floatingDiv" style={{maxHeight: '500px'}}>
+                        <h3>Personal Stocks</h3>
+                        <hr/>
+                        <div id="personal-stocks" className="overflow-auto" style={{ maxHeight: '400px' }}>
+                            {stockData.map((stock, index) => (
+                                <div key={index} className="floatingDiv m-auto my-2" style={{ minWidth: '200px', width: '200px' }}>
+                                    <p>{stock.ticker}</p>
+                                    <p>${stock.price.toFixed(2)}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-10 col-sm-12"> {/* adjust the size if u want */}
                     <div className='floatingDiv'>
                         <h3>Your Stocks</h3>
                         <hr/>
                         <StockGraph />
                     </div>
                 </div>
-                <div className="my-3 col-lg-2 col-sm-12">
-                    <div className="floatingDiv" style={{maxHeight: '500px'}}>
-                        <h3>Personal Stocks</h3>
-                        <hr/>
-                        <div id="personal-stocks" className="overflow-auto" style={{ maxHeight: '400px' }}>
-                            <div id="personal-stocks">
-                                {stockData.map((stock, index) => (
-                                    <div key={index} className="floatingDiv m-auto my-2" style={{ minWidth: '200px', width: '200px' }}>
-                                        <p>{stock.ticker}</p>
-                                        <p>${stock.price.toFixed(2)}</p>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
+        </div>
     );
 }
 

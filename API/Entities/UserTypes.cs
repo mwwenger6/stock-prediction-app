10ef8931871e4bef9b2a71f655b15fbeaecde8bf
Thomson Toms <tomsthomson1@gmail.com>
2024-01-27 14:32:56 -0500

    sopme .cs fioles

---
+++ API/Entities/UserTypes.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Stock_Prediction_API.Entities
+{
+    [Table("UserTypes", Schema = "dbo")]
+    public class UserType
+    {
+#nullable disable
+        [Column("UserTypeId")]
+        public int Id { get; set; }
+        [Column("UserTypeName")]
+        public string UserTypeName { get; set; }
+        
+    }
+}

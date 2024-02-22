3b2fece5f3bb08ffb6834ba8a687dce457dc0c1e
Peter Dizor <pcdizor@gmail.com>
2024-02-22 07:17:13 -0500

    updated reuirements.txt

--- API/obj/Debug/net7.0/Stock-Prediction-API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/Stock-Prediction-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stock-Prediction-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d2295dd23b4b6528d87e6ba7d89b970da62a6e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stock-Prediction-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stock-Prediction-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

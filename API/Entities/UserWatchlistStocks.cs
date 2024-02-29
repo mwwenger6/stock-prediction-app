1a60cbecd78a939bde2837c25507ddad93704abf
jacobbianco <115517309+jacobbianco@users.noreply.github.com>
2024-02-28 22:08:20 -0500

    Add files via upload

---
+++ API/Entities/UserWatchlistStocks.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Stock_Prediction_API.Entities
+{
+    [Table("UserWatchlistStocks", Schema = "dbo")]
+    public class UserWatchlistStocks
+    {
+#nullable disable
+        [Column("WatchlistId")]
+        public int Id { get; set; }
+        [Column("UserId")]
+        public int UserId { get; set; }
+        [Column("Ticker")]
+        public string Ticker { get; set; }
+    }
+}

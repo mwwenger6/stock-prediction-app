b01c6a8f8c199678f430488ed7e81c60a3a6d0d3
Jacob Bianco <jacobbianco377@gmail.com>
2024-03-09 00:32:02 -0500

    Merge and try to fix config file

--- Front_End/src/config.ts
+++ Front_End/src/config.ts
@@ -15,20 +15,15 @@ const endpoints = {
     getStocks: `${API_BASE_URL}/Home/GetStocks`,
     getStockData:(ticker : string) => `${API_BASE_URL}/Home/GetStock/${ticker}`,
     getHistStockData: (ticker : string) => `${API_BASE_URL}/Home/GetHistoricalStockData/${ticker}`,
-<<<<<<< HEAD
     trainModel: (ticker: string) => `${API_PRED_URL}/Home/TrainModel/${ticker}`,
     getPredictions: (ticker : string, date : Date) => `${API_BASE_URL}/Home/GetPredictions/${ticker}/${date}`,
-    predict: () => `${API_PRED_URL}/Home/AddPredictions`,
-    getUsers: `${API_BASE_URL}/Home/GetUsers`, // Added endpoint for fetching users
-=======
+    predict: (ticker : string, range : number) => `${API_PRED_URL}/Home/Predict/${ticker}/${range}`,
     getOpenMarketDays: (num : number) => `${API_BASE_URL}/Home/GetOpenMarketDays/${num}`,
 
     //Admin Endpoints
     getErrorLogs: `${API_BASE_URL}/Home/GetErrorLogs`,
-    trainModel: (ticker: string) => `${API_BASE_URL}/Home/TrainModel/${ticker}`,
-    predict: (ticker : string, range : number) => `${API_BASE_URL}/Home/Predict/${ticker}/${range}`,
 
->>>>>>> e04e0bd (Stock watchlist feature fully implemented)
+
     // Add more endpoints as needed
 };
 
